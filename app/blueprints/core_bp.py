a9729f31453cabbf991c9960fc67386e6f6549cb
Juacy Willian <juacy.willian@gmail.com>
2022-06-01 09:48:15 -0300

    adicionando imagens, e estilos basicos a pagina do blog

--- app/blueprints/core_bp.py
+++ app/blueprints/core_bp.py
@@ -5,5 +5,7 @@ core_bp = Blueprint('core', __name__)
 
 @core_bp.route('/')
 def index():
-    return render_template('index.html')
+
+    return render_template('index.html', 
+        name="Juacy Willian", username="juacywillian")
     
