7d84cbdd9a20acfd03806c3fa80d4959c81a9c97
Juacy Willian <juacy.willian@gmail.com>
2022-06-02 11:00:30 -0300

    atualizando o template

--- app/blueprints/core_bp.py
+++ app/blueprints/core_bp.py
@@ -1,11 +1,12 @@
 from distutils import core
 from flask import Blueprint, render_template
 
+from ..utils import posts
+
 core_bp = Blueprint('core', __name__)
 
 @core_bp.route('/')
 def index():
-
     return render_template('index.html', 
-        name="Juacy Willian", username="juacywillian")
+        name="Juacy Willian", username="juacywillian", posts=posts)
     
