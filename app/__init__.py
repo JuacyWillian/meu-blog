27513bb907403861bc9f0c105d294326f69181bf
Juacy Willian <juacy.willian@gmail.com>
2022-06-08 13:29:53 -0300

    installing markdown

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flaskext.markdown import Markdown
+
 
 from . import config, models, blueprints
+
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object(config)
+
+    Markdown(app)
+
     models.configure(app)
     blueprints.configure(app)
-
 
     return app
