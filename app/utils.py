ba24f55a4590339db7ec5c1f2452df1d99371f24
Juacy Willian <juacy.willian@gmail.com>
2022-06-08 10:48:23 -0300

    adicionando texto mais longo com MussumIpsum

--- app/utils.py
+++ app/utils.py
@@ -3,8 +3,17 @@
 posts = (
     {
         "title": "What is a Perfect Blog Post? Example + 5 Steps to Create One.",
-        "body": """Creating blog posts that perform can seem like a game of roulette: sometimes you win, most times you don’t. But by employing the right strategies, you can increase performance consistently and turn gambles into sure bets. See how we do it in this blog post example.
+        "body": """
+        Mussum Ipsum, cacilds vidis litro abertis. A ordem dos tratores não altera o pão duris.Sapien in monti palavris qui num significa nadis i pareci latim.Manduma pindureta quium dia nois paga.Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.
+
+        Pra lá , depois divoltis porris, paradis.Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+
+        Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!Quem num gosta di mim que vai caçá sua turmis!Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.
+
+        Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.
+
+        Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!Si num tem leite então bota uma pinga aí cumpadi!Não sou faixa preta cumpadi, sou preto inteiris, inteiris.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.
         """,
-        "dt":"1 de jun de 2022"
+        "dt": "1 de jun de 2022"
     },
 )
