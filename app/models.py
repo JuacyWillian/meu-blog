672c3252cc39f19fcde4b90f25a7e120d76c314f
Juacy Willian <juacy.willian@gmail.com>
2022-06-26 10:28:16 -0300

    creating Category, Post and User basic models

--- app/models.py
+++ app/models.py
@@ -1,7 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import nullslast
 
 db = SQLAlchemy()
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String)
+    # posts=
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    # categories=db.
+    # tags=db.
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    __password = db.Column(db.String, nullable=False)
+    # posts= db
 
 
 def configure(app: Flask):
